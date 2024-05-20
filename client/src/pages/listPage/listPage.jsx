035dfb8afc559b47e3d277f1b0c15a6b38c91d89
hwrdyen <hwrdpvt@gmail.com>
2024-05-20 13:55:38 +0800

    Initiated listPage info-card, list, and searchBar

--- client/src/pages/listPage/listPage.jsx
+++ client/src/pages/listPage/listPage.jsx
@@ -1,7 +1,24 @@
+import { listData } from "../../lib/dummyData";
 import "./listPage.scss";
+import Filter from "../../components/filter/filter";
+import Card from "../../components/card/card";
 
 function ListPage() {
-  return <div className="listPage">ListPage</div>;
+  const data = listData;
+
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer"></div>
+    </div>
+  );
 }
 
 export default ListPage;
