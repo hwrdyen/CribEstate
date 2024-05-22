1e8ffd35036fc307fd63d8e474b34564e8f134d2
hwrdyen <hwrdpvt@gmail.com>
2024-05-22 11:28:57 +0800

    Added update-profile-pic functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -47,7 +47,9 @@ export const updateUser = async (req, res) => {
         ...(updatedPassword && { password: updatedPassword }),
       },
     });
-    res.status(200).json(updatedUser);
+
+    const { password: userPassword, ...rest } = updatedUser;
+    res.status(200).json(rest);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to update user!" });
--- client/src/pages/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/pages/profileUpdatePage/profileUpdatePage.jsx
@@ -1,15 +1,35 @@
 import "./profileUpdatePage.scss";
-import { useContext } from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
+import UploadWidget from "../../components/uploadWidget/uploadWidget";
 
 function ProfileUpdatePage() {
   const { updateUser, currentUser } = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [avatarImg, setAvatarImg] = useState(currentUser.avatar);
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
 
     const { username, email, password } = Object.fromEntries(formData); //different method than Login.jsx
+
+    try {
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {
+        username,
+        email,
+        password,
+        avatar: avatarImg,
+      });
+      updateUser(res.data);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
+    }
   };
 
   return (
@@ -40,13 +60,24 @@ function ProfileUpdatePage() {
             <input id="password" name="password" type="password" />
           </div>
           <button>Update</button>
+          {error && <span>{error}</span>}
         </form>
       </div>
       <div className="sideContainer">
+        {console.log(currentUser.avatar)}
         <img
-          src={currentUser.avatar || "/noavatar.jpg"}
+          src={avatarImg || currentUser.avatar || "/noavatar.jpg"}
           alt=""
           className="avatar"
+        />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "howiedev",
+            uploadPreset: "CribEstate",
+            multiple: false,
+            folder: "avatars",
+          }}
+          setAvatarImg={setAvatarImg}
         />
       </div>
     </div>
